e7a22732886441856ddca9f4dc0a1697b05fea6f
KenjyHD <liviu.calmis@mail.ru>
2024-05-24 18:11:31 +0300

    Folder rename

--- book-api/src/main/java/com/kenjy/bookapi/BookApiApplication.java
+++ book-api/src/main/java/com/kenjy/bookapi/BookApiApplication.java
@@ -1,4 +1,4 @@
-package com.ivanfranchin.bookapi;
+package com.kenjy.bookapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- book-api/src/test/java/com/kenjy/bookapi/BookApiApplicationTests.java
+++ book-api/src/test/java/com/kenjy/bookapi/BookApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ivanfranchin.bookapi;
+package com.kenjy.bookapi;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
