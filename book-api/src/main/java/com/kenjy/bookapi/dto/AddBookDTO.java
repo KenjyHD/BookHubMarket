f21a00874c6508eac42b5cabed30bd06c2515532
KenjyHD <liviu.calmis@mail.ru>
2024-05-24 18:00:53 +0300

    Added dto for adding book

---
+++ book-api/src/main/java/com/kenjy/bookapi/dto/AddBookDTO.java
@@ -0,0 +1,25 @@
+package com.kenjy.bookapi.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddBookDTO {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Author is required")
+    private String author;
+
+    @NotBlank(message = "Price is required")
+    private Float price;
+
+    private String genre;
+
+    private String description;
+}
