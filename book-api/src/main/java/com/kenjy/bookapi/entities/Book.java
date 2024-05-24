3257dd4d8d0eb048481c3825612b183137ecd2c0
KenjyHD <liviu.calmis@mail.ru>
2024-05-24 18:10:28 +0300

    Renamed model folder to entities; added book one-to-many relationship with user books and purchase request tables

---
+++ book-api/src/main/java/com/kenjy/bookapi/entities/Book.java
@@ -0,0 +1,50 @@
+package com.kenjy.bookapi.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String author;
+
+    @NotNull
+    private Float price;
+
+    private String genre;
+
+    private String description;
+
+    @NotNull
+    private String bookPdfPath;
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UsersBooks> usersBooks = new HashSet<>();
+
+    @OneToMany(mappedBy = "book", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<PurchaseRequest> purchaseRequests = new HashSet<>();
+
+    public Book(String title, String author, Float price, String bookPdfPath) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.bookPdfPath = bookPdfPath;
+    }
+}
