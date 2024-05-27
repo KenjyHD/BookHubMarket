23a379bcb7eec0b0cfde37b32d0012d146292c29
KenjyHD <liviu.calmis@mail.ru>
2024-05-27 23:28:02 +0300

    Folder renamed

--- book-api/src/main/java/com/kenjy/bookapi/exception/DuplicatedUserInfoException.java
+++ book-api/src/main/java/com/kenjy/bookapi/exception/DuplicatedUserInfoException.java
@@ -1,4 +1,4 @@
-package com.ivanfranchin.bookapi.exception;
+package com.kenjy.bookapi.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
