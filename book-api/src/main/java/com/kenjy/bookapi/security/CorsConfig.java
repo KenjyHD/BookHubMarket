e8fd5bb8c563c34b8bac09d8331267d65e86bde7
KenjyHD <liviu.calmis@mail.ru>
2024-05-27 21:21:12 +0300

    Folder renamed

--- book-api/src/main/java/com/kenjy/bookapi/security/CorsConfig.java
+++ book-api/src/main/java/com/kenjy/bookapi/security/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.ivanfranchin.bookapi.security;
+package com.kenjy.bookapi.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
