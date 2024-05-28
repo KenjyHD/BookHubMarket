42cafa1631a4cfb0cfe7e50c281f36244bf5fda3
KenjyHD <liviu.calmis@mail.ru>
2024-05-28 17:14:54 +0300

    Added new entity for processing purchase requests

---
+++ book-api/src/main/java/com/kenjy/bookapi/entities/PurchaseRequest.java
@@ -0,0 +1,44 @@
+package com.kenjy.bookapi.entities;
+
+import com.kenjy.bookapi.enums.PurchaseRequestStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "purchase_requests",
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "book_id"})})
+public class PurchaseRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PurchaseRequestStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime requestDate;
+
+    private LocalDateTime decisionDate;
+
+    public PurchaseRequest(User user, Book book, PurchaseRequestStatus status, LocalDateTime requestDate) {
+        this.user = user;
+        this.book = book;
+        this.status = status;
+        this.requestDate = requestDate;
+    }
+}
