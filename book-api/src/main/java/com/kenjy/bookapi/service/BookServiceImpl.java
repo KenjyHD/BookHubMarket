06c402b407777aaf801a9cd0bee23b5d46d439bf
KenjyHD <liviu.calmis@mail.ru>
2024-05-28 22:19:11 +0300

    Added methods for downloading book pdf and getting list of purchased books for a given user id

--- book-api/src/main/java/com/kenjy/bookapi/service/BookServiceImpl.java
+++ book-api/src/main/java/com/kenjy/bookapi/service/BookServiceImpl.java
@@ -1,42 +1,76 @@
 package com.kenjy.bookapi.service;
 
-import com.kenjy.bookapi.entities.Book;
+import com.kenjy.bookapi.entities.Books;
+import com.kenjy.bookapi.entities.UsersBooks;
 import com.kenjy.bookapi.exception.BookNotFoundException;
 import com.kenjy.bookapi.repository.BookRepository;
+import com.kenjy.bookapi.repository.UsersBooksRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
 
     private final BookRepository bookRepository;
+    private final UsersBooksRepository usersBooksRepository;
 
     @Override
-    public List<Book> getBooks() {
+    public List<Books> getBooks() {
         return bookRepository.findAllByOrderByTitle();
     }
 
     @Override
-    public List<Book> getBooksContainingText(String text) {
+    public List<Books> getBooksContainingText(String text) {
         return bookRepository.findByTitleContainingIgnoreCaseOrderByTitle(text);
     }
 
     @Override
-    public Book validateAndGetBook(Long id) {
+    public Books validateAndGetBook(Long id) {
         return bookRepository.findById(id)
-                .orElseThrow(() -> new BookNotFoundException(String.format("Book with id %s not found", id)));
+                .orElseThrow(() -> new BookNotFoundException(String.format("Books with id %s not found", id)));
     }
 
     @Override
-    public Book saveBook(Book book) {
+    public Books saveBook(Books book) {
         return bookRepository.save(book);
     }
 
     @Override
-    public void deleteBook(Book book) {
+    public void deleteBook(Books book) {
         bookRepository.delete(book);
+    }
+
+    @Override
+    public Resource getBookPdf(Long bookId) {
+        try {
+            Books book = bookRepository.findById(bookId)
+                    .orElseThrow(() -> new RuntimeException("Books not found"));
+
+            Path filePath = Paths.get(book.getBookPdfPath()).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("File not found or not readable");
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Error while trying to load file", ex);
+        }
+    }
+
+    public List<Books> getPurchasedBooks(Long userId) {
+        List<UsersBooks> usersBooks = usersBooksRepository.findByUserId(userId);
+        return usersBooks.stream()
+                .map(UsersBooks::getBook)
+                .collect(Collectors.toList());
     }
 }
