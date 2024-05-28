0845d1d23b10a34135810520677760debbe9561a
KenjyHD <liviu.calmis@mail.ru>
2024-05-28 22:18:13 +0300

    Changed id generation strategy to IDENTITY

--- book-api/src/main/java/com/kenjy/bookapi/entities/Books.java
+++ book-api/src/main/java/com/kenjy/bookapi/entities/Books.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Table(name = "books")
 public class Books {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
