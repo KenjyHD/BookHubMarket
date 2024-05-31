a883ab741754e61b2bc9c075614dcd443d2c91c1
KenjyHD <liviu.calmis@mail.ru>
2024-05-31 17:44:46 +0300

    Added dto for updating users info

---
+++ book-api/src/main/java/com/kenjy/bookapi/dto/UpdateUserDTO.java
@@ -0,0 +1,15 @@
+package com.kenjy.bookapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UpdateUserDTO {
+    private String username;
+    private String name;
+    private String email;
+    private String password;
+}
