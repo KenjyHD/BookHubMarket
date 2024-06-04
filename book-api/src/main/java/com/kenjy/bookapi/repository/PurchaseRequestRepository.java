6e8031a03e191fb8904c83a6bf44e9666cd2b23f
KenjyHD <liviu.calmis@mail.ru>
2024-06-04 17:52:58 +0300

    Added method for checking purchase request existence by user id, book id and status; method for getting the purchase request status by the given user and book id

--- book-api/src/main/java/com/kenjy/bookapi/repository/PurchaseRequestRepository.java
+++ book-api/src/main/java/com/kenjy/bookapi/repository/PurchaseRequestRepository.java
@@ -2,8 +2,15 @@ package com.kenjy.bookapi.repository;
 
 
 import com.kenjy.bookapi.entities.PurchaseRequest;
+import com.kenjy.bookapi.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest, Long> {
     boolean existsByUserIdAndBookId(Long userId, Long bookId);
+
+    boolean existsByUserIdAndBookIdAndStatus(Long userId, Long bookId, RequestStatus status);
+
+    @Query("SELECT pr.status FROM PurchaseRequest pr WHERE pr.user.id = :userId AND pr.book.id = :bookId")
+    RequestStatus findStatusByUserIdAndBookId(Long userId, Long bookId);
 }
