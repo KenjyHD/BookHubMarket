e05dfe53a607a5b9b8041131b158d01d4d889cc6
KenjyHD <liviu.calmis@mail.ru>
2024-06-04 17:45:50 +0300

    Added method for getting books owned by certain user and with title containing the given string

--- book-api/src/main/java/com/kenjy/bookapi/repository/UsersBooksRepository.java
+++ book-api/src/main/java/com/kenjy/bookapi/repository/UsersBooksRepository.java
@@ -1,10 +1,15 @@
 package com.kenjy.bookapi.repository;
 
+import com.kenjy.bookapi.entities.Book;
 import com.kenjy.bookapi.entities.UsersBooks;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface UsersBooksRepository extends JpaRepository<UsersBooks, Long> {
     List<UsersBooks> findByUserId(Long userId);
+    @Query("SELECT ub.book FROM UsersBooks ub WHERE ub.user.id = :userId AND LOWER(ub.book.title) LIKE LOWER(CONCAT('%', :text, '%')) ORDER BY ub.book.title")
+    List<Book> findByUserIdAndBookTitleContainingIgnoreCaseOrderByTitle(@Param("userId") Long userId, @Param("text") String text);
 }
