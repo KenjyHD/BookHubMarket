a2d86333ae882be9b50add1e73a717c160ce99a4
KenjyHD <liviu.calmis@mail.ru>
2024-06-06 18:56:49 +0300

    Test books now their author set as the admin, added request_date for test purchase requests

--- book-api/src/main/resources/db/migration/V9__ADD_TEST_DATA.sql
+++ book-api/src/main/resources/db/migration/V9__ADD_TEST_DATA.sql
@@ -4,10 +4,10 @@ INSERT INTO users (username, password, name, email, role) VALUES
 
 -- Insert test data into book_content
 INSERT INTO book_contents (id, file_name, mime_type) VALUES
-    (gen_random_uuid(), 'content1.pdf', 'application/pdf'),
-    (gen_random_uuid(), 'content2.pdf', 'application/pdf'),
-    (gen_random_uuid(), 'content3.pdf', 'application/pdf'),
-    (gen_random_uuid(), 'content4.pdf', 'application/pdf');
+    ('9023ac6a-566d-4ce2-889b-528a34ed59f0', 'content1.pdf', 'application/pdf'),
+    ('9ba912b5-a74e-4445-936c-1c17f2819c06', 'content2.pdf', 'application/pdf'),
+    ('25030690-5861-49e3-9236-bd5e202c44d6', 'content3.pdf', 'application/pdf'),
+    ('0ece6d06-ff63-49c4-82ce-db03cc2bfec1', 'content4.pdf', 'application/pdf');
 
 -- Insert test data into book_cover
 INSERT INTO book_covers (id, file_name, mime_type) VALUES
@@ -17,28 +17,30 @@ INSERT INTO book_covers (id, file_name, mime_type) VALUES
     ('ae1c3124-57a0-4b51-91f8-b1df2172499f', 'The Barefoot Serpent.jpg', 'image/jpeg');
 
 -- Insert test data into books
-INSERT INTO books (title, author, price, genre, description, book_content_id, book_cover_id) VALUES
+INSERT INTO books (title, author, price, genre, description, author_id, book_content_id, book_cover_id) VALUES
     ('Any Empire', 'author1', 12.3, 'Genre1', 'Description1',
+    (SELECT id FROM users WHERE username = 'admin'),
     (SELECT id FROM book_contents LIMIT 1 OFFSET 0),
     (SELECT id FROM book_covers LIMIT 1 OFFSET 0)),
     ('August Moon', 'author2', 12.3, 'Genre2', 'Description2',
+    (SELECT id FROM users WHERE username = 'admin'),
     (SELECT id FROM book_contents LIMIT 1 OFFSET 1),
     (SELECT id FROM book_covers LIMIT 1 OFFSET 1)),
     ('The Barefoot Serpent', 'author3', 12.3, 'Genre3', 'Description3',
+    (SELECT id FROM users WHERE username = 'admin'),
     (SELECT id FROM book_contents LIMIT 1 OFFSET 2),
     (SELECT id FROM book_covers LIMIT 1 OFFSET 2)),
     ('BB Wolf and the 3 LPs', 'author4', 12.3, 'Genre4', 'Description4',
+    (SELECT id FROM users WHERE username = 'admin'),
     (SELECT id FROM book_contents LIMIT 1 OFFSET 3),
     (SELECT id FROM book_covers LIMIT 1 OFFSET 3));
 
 INSERT INTO users_books (user_id, book_id) VALUES
-    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'Any Empire')),
-    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'August Moon')),
     ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'The Barefoot Serpent')),
     ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'BB Wolf and the 3 LPs'));
 
-INSERT INTO purchase_requests (user_id, book_id, status) VALUES
-    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'Any Empire'), 'APPROVED'),
-    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'August Moon'), 'APPROVED'),
-    ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'The Barefoot Serpent'), 'APPROVED'),
-    ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'BB Wolf and the 3 LPs'), 'APPROVED');
+INSERT INTO purchase_requests (user_id, book_id, status, request_date) VALUES
+    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'Any Empire'), 'APPROVED', CURRENT_TIMESTAMP),
+    ((SELECT id FROM users WHERE username = 'admin'), (SELECT id FROM books WHERE title = 'August Moon'), 'APPROVED', CURRENT_TIMESTAMP),
+    ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'The Barefoot Serpent'), 'APPROVED', CURRENT_TIMESTAMP),
+    ((SELECT id FROM users WHERE username = 'user'), (SELECT id FROM books WHERE title = 'BB Wolf and the 3 LPs'), 'APPROVED', CURRENT_TIMESTAMP);
