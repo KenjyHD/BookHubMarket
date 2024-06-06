3731c9ccc6c4647d1e36fb904f0480082752748f
KenjyHD <liviu.calmis@mail.ru>
2024-06-06 18:55:15 +0300

    Now book author fk is related to users table

--- book-api/src/main/resources/db/migration/V5__CREATE_BOOKS_TABLE.sql
+++ book-api/src/main/resources/db/migration/V5__CREATE_BOOKS_TABLE.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS books (
     price FLOAT NOT NULL,
     genre VARCHAR(255),
     description TEXT,
-    FOREIGN KEY (author_id) REFERENCES authors(id),
+    FOREIGN KEY (author_id) REFERENCES users(id),
     FOREIGN KEY (book_content_id) REFERENCES book_contents(id),
     FOREIGN KEY (book_cover_id) REFERENCES book_covers(id)
 );
