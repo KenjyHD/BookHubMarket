a1d7e0057aaa1acbcae1cf62c3d5806a7f9f0644
KenjyHD <liviu.calmis@mail.ru>
2024-06-07 18:24:40 +0300

    When deleting a book or a user, all the dependent foreign key elements will also be deleted on cascade

--- book-api/src/main/resources/db/migration/V6__CREATE_AUTHOR_REQUESTS_TABLE.sql
+++ book-api/src/main/resources/db/migration/V6__CREATE_AUTHOR_REQUESTS_TABLE.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS author_requests (
     status VARCHAR(50) NOT NULL,
     request_date TIMESTAMP NOT NULL,
     decision_date TIMESTAMP,
-    CONSTRAINT fk_author_requests_users FOREIGN KEY (user_id) REFERENCES users (id),
-    CONSTRAINT fk_author_requests_books FOREIGN KEY (book_id) REFERENCES books (id),
+    CONSTRAINT fk_author_requests_users FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_author_requests_books FOREIGN KEY (book_id) REFERENCES books (id) ON DELETE CASCADE,
     CONSTRAINT unique_user_book_author_requests UNIQUE (user_id, book_id)
 );
