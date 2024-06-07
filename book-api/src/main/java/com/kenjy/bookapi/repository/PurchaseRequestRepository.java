ebede20121d59af731dc9c2f51f041be6c04ecb0
KenjyHD <liviu.calmis@mail.ru>
2024-06-07 18:46:41 +0300

    Added method for getting all purchase requests by the given author (user) id

--- book-api/src/main/java/com/kenjy/bookapi/repository/PurchaseRequestRepository.java
+++ book-api/src/main/java/com/kenjy/bookapi/repository/PurchaseRequestRepository.java
@@ -6,10 +6,14 @@ import com.kenjy.bookapi.enums.RequestStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface PurchaseRequestRepository extends JpaRepository<PurchaseRequest, Long> {
     boolean existsByUserIdAndBookId(Long userId, Long bookId);
 
     boolean existsByUserIdAndBookIdAndStatus(Long userId, Long bookId, RequestStatus status);
+
+    List<PurchaseRequest> findAllByBookAuthorId(Long authorId);
 
     @Query("SELECT pr.status FROM PurchaseRequest pr WHERE pr.user.id = :userId AND pr.book.id = :bookId")
     RequestStatus findStatusByUserIdAndBookId(Long userId, Long bookId);
