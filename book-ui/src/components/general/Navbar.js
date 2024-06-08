8baad2e4113af6f3f7b777417c951f47758b8333
KenjyHD <liviu.calmis@mail.ru>
2024-06-08 17:26:28 +0300

    Renamed navbar styles, removed a redundant style

--- book-ui/src/components/general/Navbar.js
+++ book-ui/src/components/general/Navbar.js
@@ -10,30 +10,25 @@ function Navbar() {
     userLogout()
   }
 
-  const enterMenuStyle = () => {
+  const unauthorizedStyle = () => {
     return userIsAuthenticated() ? { "display": "none" } : { "display": "block" }
   }
 
-  const logoutMenuStyle = () => {
+  const authorizedStyle = () => {
     return userIsAuthenticated() ? { "display": "block" } : { "display": "none" }
   }
 
-  const adminPageStyle = () => {
+  const adminNavbarStyle = () => {
     const user = getUser()
     return user && user.role === 'ADMIN' ? { "display": "block" } : { "display": "none" }
   }
 
-  const purchaseRequestsPageStyle = () => {
+  const adminAuthorNavbarStyle = () => {
     const user = getUser()
     return user && ['ADMIN', 'AUTHOR'].includes(user.role) ? { "display": "block" } : { "display": "none" }
   }
 
-  const libraryStyle = () => {
-    const user = getUser()
-    return user && ['USER', 'AUTHOR'].includes(user.role) ? { "display": "block" } : { "display": "none" }
-  }
-
-  const personalLibraryStyle = () => {
+  const authorUserStyle = () => {
     const user = getUser()
     return user && ['USER', 'AUTHOR'].includes(user.role) ? { "display": "block" } : { "display": "none" }
   }
@@ -48,16 +43,17 @@ function Navbar() {
       <Container>
         <Menu.Item header>Book-UI</Menu.Item>
         <Menu.Item as={Link} exact='true' to="/">Home</Menu.Item>
-        <Menu.Item as={Link} to="/adminpage" style={adminPageStyle()}>AdminPage</Menu.Item>
-        <Menu.Item as={Link} to="/library" style={libraryStyle()}>Library</Menu.Item>
-        <Menu.Item as={Link} to="/personal-library" style={personalLibraryStyle()}>My Books</Menu.Item>
-        <Menu.Item as={Link} to="/purchase-requests" style={purchaseRequestsPageStyle()}>Purchase Requests</Menu.Item>
+        <Menu.Item as={Link} to="/adminpage" style={adminNavbarStyle()}>AdminPage</Menu.Item>
+        <Menu.Item as={Link} to="/library" style={authorUserStyle()}>Library</Menu.Item>
+        <Menu.Item as={Link} to="/personal-library" style={authorUserStyle()}>My Books</Menu.Item>
+        <Menu.Item as={Link} to="/purchase-requests" style={adminAuthorNavbarStyle()}>Purchase Requests</Menu.Item>
+        <Menu.Item as={Link} to="/author-requests" style={adminNavbarStyle()}>Author Requests</Menu.Item>
         <Menu.Menu position='right'>
-          <Menu.Item as={Link} to="/login" style={enterMenuStyle()}>Login</Menu.Item>
-          <Menu.Item as={Link} to="/signup" style={enterMenuStyle()}>Sign Up</Menu.Item>
-          <Menu.Item as={Link} to="/profile" style={logoutMenuStyle()} icon='user' />
-          <Menu.Item header style={logoutMenuStyle()}>{`Hi ${getUserName()}`}</Menu.Item>
-          <Menu.Item as={Link} to="/" style={logoutMenuStyle()} onClick={logout}>Logout</Menu.Item>
+          <Menu.Item as={Link} to="/login" style={unauthorizedStyle()}>Login</Menu.Item>
+          <Menu.Item as={Link} to="/signup" style={unauthorizedStyle()}>Sign Up</Menu.Item>
+          <Menu.Item as={Link} to="/profile" style={authorizedStyle()} icon='user' />
+          <Menu.Item header style={authorizedStyle()}>{`Hi ${getUserName()}`}</Menu.Item>
+          <Menu.Item as={Link} to="/" style={authorizedStyle()} onClick={logout}>Logout</Menu.Item>
         </Menu.Menu>
       </Container>
     </Menu>
