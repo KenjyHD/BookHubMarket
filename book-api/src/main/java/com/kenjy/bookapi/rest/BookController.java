f8aa5c248c23890b0e9ea41fccc17f42d8bb3ac3
KenjyHD <liviu.calmis@mail.ru>
2024-06-08 17:32:40 +0300

    Moved implementation to service, added endpoints for getting all purchased books for a given user id, another endpoint for getting all books posted by a given author (user) id

--- book-api/src/main/java/com/kenjy/bookapi/rest/BookController.java
+++ book-api/src/main/java/com/kenjy/bookapi/rest/BookController.java
@@ -1,28 +1,27 @@
 package com.kenjy.bookapi.rest;
 
-import com.kenjy.bookapi.entities.Books;
-import com.kenjy.bookapi.mapper.BookMapper;
+import com.kenjy.bookapi.config.SwaggerConfig;
 import com.kenjy.bookapi.dto.AddBookDTO;
 import com.kenjy.bookapi.dto.GetBookDTO;
+import com.kenjy.bookapi.entities.Book;
+import com.kenjy.bookapi.entities.BookContent;
+import com.kenjy.bookapi.entities.User;
+import com.kenjy.bookapi.mapper.BookMapper;
 import com.kenjy.bookapi.service.BookService;
-import com.kenjy.bookapi.config.SwaggerConfig;
+import com.kenjy.bookapi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,67 +29,69 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api/books")
 public class BookController {
-
     private final BookService bookService;
+    private final UserService userService;
     private final BookMapper bookMapper;
-    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
-    private final String uploadDir = System.getProperty("user.dir") + "\\book-api\\src\\main\\resources\\file-storage\\";
 
     @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
     @GetMapping
     public List<GetBookDTO> getBooks(@RequestParam(value = "text", required = false) String text) {
-        List<Books> books = (text == null) ? bookService.getBooks() : bookService.getBooksContainingText(text);
+        List<Book> books = (text == null) ? bookService.getBooks() : bookService.getBooksContainingText(text);
         return books.stream()
                 .map(bookMapper::toBookDTO)
                 .collect(Collectors.toList());
     }
 
     @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
+    @GetMapping("/purchased/{userId}")
+    public ResponseEntity<List<GetBookDTO>> getPurchasedBooks(
+            @PathVariable Long userId,
+            @RequestParam(value = "text", required = false) String text) {
+        return ResponseEntity.ok(bookService.getPurchasedBooks(userId, text));
+    }
+
+    @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
+    @GetMapping("/author/{userId}")
+    public ResponseEntity<List<GetBookDTO>> getAuthorBooks(
+            @PathVariable Long userId,
+            @RequestParam(value = "text", required = false) String text) {
+        return ResponseEntity.ok(bookService.getAuthorBooks(userId, text));
+    }
+
+    @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = {"multipart/form-data"})
-    public GetBookDTO createBook(@RequestPart("book") AddBookDTO dto,
-                                 @RequestPart("pdfFile") MultipartFile pdfFile) {
-        try {
-            File file = new File(uploadDir + pdfFile.getOriginalFilename());
-            try (OutputStream os = new FileOutputStream(file)) {
-                os.write(pdfFile.getBytes());
-            }
-
-            Books book = bookMapper.toBook(dto, pdfFile);
-            return bookMapper.toBookDTO(bookService.saveBook(book));
-        } catch (IOException e) {
-            logger.error("An error occurred:", e);
-            throw new RuntimeException("Failed to save PDF file");
-        }
+    public GetBookDTO createBook(@AuthenticationPrincipal UserDetails userDetails,
+                                 @RequestPart("book") AddBookDTO dto,
+                                 @RequestPart("bookContent") MultipartFile bookContentFile,
+                                 @RequestPart("bookCover") MultipartFile bookCoverFile) {
+        User user = userService.findByUsername(userDetails.getUsername());
+        return bookService.createBook(user, dto, bookContentFile, bookCoverFile);
     }
 
     @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
     @DeleteMapping("/{id}")
     public GetBookDTO deleteBook(@PathVariable Long id) {
-        Books book = bookService.validateAndGetBook(id);
+        Book book = bookService.validateAndGetBook(id);
         bookService.deleteBook(book);
         return bookMapper.toBookDTO(book);
     }
 
     @Operation(security = {@SecurityRequirement(name = SwaggerConfig.BASIC_AUTH_SECURITY_SCHEME)})
     @GetMapping("/{id}")
-    public GetBookDTO getBook(@PathVariable Long id) {
-        Books book = bookService.validateAndGetBook(id);
-        return bookMapper.toBookDTO(book);
-    }
-
-    @GetMapping("/purchased/{userId}")
-    public ResponseEntity<List<Books>> getPurchasedBooks(@PathVariable Long userId) {
-        List<Books> purchasedBooks = bookService.getPurchasedBooks(userId);
-        return ResponseEntity.ok(purchasedBooks);
+    public ResponseEntity<GetBookDTO> getBook(@PathVariable Long id) {
+        Book book = bookService.validateAndGetBook(id);
+        return ResponseEntity.ok(bookMapper.toBookDTO(book));
     }
 
     @GetMapping("/{bookId}/download")
     public ResponseEntity<Resource> downloadBook(@PathVariable Long bookId) {
-        Resource resource = bookService.getBookPdf(bookId);
+        Resource resource = bookService.getBookContent(bookId);
+        BookContent bookContent = bookService.getBookContentDetails(bookId);
+
         return ResponseEntity.ok()
-                .contentType(MediaType.APPLICATION_PDF)
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"book_" + bookId + ".pdf\"")
+                .contentType(MediaType.parseMediaType(bookContent.getMimeType()))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + bookContent.getFileName() + "\"")
                 .body(resource);
     }
 
