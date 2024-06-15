092187194d6c310f608ee1d626ef829e7bfa2cdd
KenjyHD <liviu.calmis@mail.ru>
2024-06-15 19:45:40 +0300

    Modified test files uuid

--- book-api/src/main/resources/db/migration/V9__ADD_TEST_DATA.sql
+++ book-api/src/main/resources/db/migration/V9__ADD_TEST_DATA.sql
@@ -5,17 +5,17 @@ INSERT INTO users (username, password, name, email, role) VALUES
 
 -- Insert test data into book_content
 INSERT INTO book_contents (id, file_name, mime_type) VALUES
-    ('9023ac6a-566d-4ce2-889b-528a34ed59f0', 'content1.pdf', 'application/pdf'),
-    ('9ba912b5-a74e-4445-936c-1c17f2819c06', 'content2.pdf', 'application/pdf'),
-    ('25030690-5861-49e3-9236-bd5e202c44d6', 'content3.pdf', 'application/pdf'),
-    ('0ece6d06-ff63-49c4-82ce-db03cc2bfec1', 'content4.pdf', 'application/pdf');
+    ('00000000-0000-0000-0000-000000000001', 'content1.pdf', 'application/pdf'),
+    ('00000000-0000-0000-0000-000000000002', 'content2.pdf', 'application/pdf'),
+    ('00000000-0000-0000-0000-000000000003', 'content3.pdf', 'application/pdf'),
+    ('00000000-0000-0000-0000-000000000004', 'content4.pdf', 'application/pdf');
 
 -- Insert test data into book_cover
 INSERT INTO book_covers (id, file_name, mime_type) VALUES
-    ('c96d2448-f18a-4397-ba71-44e3a9768aa4', 'Any Empire.png', 'image/png'),
-    ('5a73a769-dac6-4c7b-a3ea-b4cd5fbf8e14', 'August Moon.jpg', 'image/jpeg'),
-    ('76347db0-0555-495a-9644-70af57d07274', 'BB Wolf and the 3 LPs.jpg', 'image/jpeg'),
-    ('ae1c3124-57a0-4b51-91f8-b1df2172499f', 'The Barefoot Serpent.jpg', 'image/jpeg');
+    ('00000000-0000-0000-0000-000000000001', 'Any Empire.png', 'image/png'),
+    ('00000000-0000-0000-0000-000000000002', 'August Moon.jpg', 'image/jpeg'),
+    ('00000000-0000-0000-0000-000000000003', 'BB Wolf and the 3 LPs.jpg', 'image/jpeg'),
+    ('00000000-0000-0000-0000-000000000004', 'The Barefoot Serpent.jpg', 'image/jpeg');
 
 -- Insert test data into books
 INSERT INTO books (title, author, price, genre, description, author_id, book_content_id, book_cover_id) VALUES
