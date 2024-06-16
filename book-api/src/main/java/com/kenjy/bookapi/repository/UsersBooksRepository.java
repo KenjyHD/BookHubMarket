c8f89f3dbc03f5031883856349b5c055c4f91d8f
KenjyHD <liviu.calmis@mail.ru>
2024-06-16 17:29:50 +0300

    Modified methods for searching books by the given search parameters, deleted unused methods

--- book-api/src/main/java/com/kenjy/bookapi/repository/UsersBooksRepository.java
+++ book-api/src/main/java/com/kenjy/bookapi/repository/UsersBooksRepository.java
@@ -9,7 +9,19 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface UsersBooksRepository extends JpaRepository<UsersBooks, Long> {
-    List<UsersBooks> findByUserId(Long userId);
-    @Query("SELECT ub.book FROM UsersBooks ub WHERE ub.user.id = :userId AND LOWER(ub.book.title) LIKE LOWER(CONCAT('%', :text, '%')) ORDER BY ub.book.title")
-    List<Book> findByUserIdAndBookTitleContainingIgnoreCaseOrderByTitle(@Param("userId") Long userId, @Param("text") String text);
+    @Query("SELECT ub.book FROM UsersBooks ub WHERE ub.user.id = :userId AND " +
+            "(:title IS NULL OR LOWER(ub.book.title) LIKE %:title%) AND " +
+            "(:genre IS NULL OR LOWER(ub.book.genre) LIKE %:genre%) AND " +
+            "(:author IS NULL OR LOWER(ub.book.authorName) LIKE %:author%) AND " +
+            "(:minPrice IS NULL OR ub.book.price >= :minPrice) AND " +
+            "(:maxPrice IS NULL OR ub.book.price <= :maxPrice) " +
+            "ORDER BY ub.book.title")
+    List<Book> findPurchasedBooks(
+            @Param("userId") Long userId,
+            @Param("title") String title,
+            @Param("genre") String genre,
+            @Param("author") String author,
+            @Param("minPrice") Float minPrice,
+            @Param("maxPrice") Float maxPrice
+    );
 }
