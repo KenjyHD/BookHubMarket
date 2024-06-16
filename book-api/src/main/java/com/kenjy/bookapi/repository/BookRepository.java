7b8b9bf16739db3b5d1149891b11a772cbed6c7a
KenjyHD <liviu.calmis@mail.ru>
2024-06-16 17:29:00 +0300

    Modified methods for searching books by the given search parameters, deleted unused methods

--- book-api/src/main/java/com/kenjy/bookapi/repository/BookRepository.java
+++ book-api/src/main/java/com/kenjy/bookapi/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.kenjy.bookapi.repository;
 
 import com.kenjy.bookapi.entities.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +15,32 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAllByTitleContainingIgnoreCaseOrderByTitle(String text);
 
-    List<Book> findAllByAuthorIdAndTitleContainingIgnoreCaseOrderByTitle(Long authorId, String text);
+    @Query("SELECT b FROM Book b WHERE " +
+            "(:title IS NULL OR LOWER(b.title) LIKE %:title%) AND " +
+            "(:genre IS NULL OR LOWER(b.genre) LIKE %:genre%) AND " +
+            "(:author IS NULL OR LOWER(b.authorName) LIKE %:author%) AND " +
+            "(:minPrice IS NULL OR b.price >= :minPrice) AND " +
+            "(:maxPrice IS NULL OR b.price <= :maxPrice)")
+    List<Book> findBooks(
+            @Param("title") String title,
+            @Param("genre") String genre,
+            @Param("author") String author,
+            @Param("minPrice") Float minPrice,
+            @Param("maxPrice") Float maxPrice
+    );
 
-    List<Book> findAllByAuthorId(Long authorId);
+    @Query("SELECT b FROM Book b WHERE b.author.id = :authorId AND " +
+            "(:title IS NULL OR LOWER(b.title) LIKE %:title%) AND " +
+            "(:genre IS NULL OR LOWER(b.genre) LIKE %:genre%) AND " +
+            "(:author IS NULL OR LOWER(b.authorName) LIKE %:author%) AND " +
+            "(:minPrice IS NULL OR b.price >= :minPrice) AND " +
+            "(:maxPrice IS NULL OR b.price <= :maxPrice)")
+    List<Book> findAuthorBooks(
+            @Param("authorId") Long authorId,
+            @Param("title") String title,
+            @Param("genre") String genre,
+            @Param("author") String author,
+            @Param("minPrice") Float minPrice,
+            @Param("maxPrice") Float maxPrice
+    );
 }
