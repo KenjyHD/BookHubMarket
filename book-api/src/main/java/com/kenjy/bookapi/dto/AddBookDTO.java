0dfece83406ba7aaeb5d19e1c1bf21c6dca65fc2
KenjyHD <calmisliviu@gmail.com>
2025-05-11 15:10:01 +0300

    Added some schema improvements

--- book-api/src/main/java/com/kenjy/bookapi/dto/AddBookDTO.java
+++ book-api/src/main/java/com/kenjy/bookapi/dto/AddBookDTO.java
@@ -1,5 +1,6 @@
 package com.kenjy.bookapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,9 +9,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Book creation DTO")
 public class AddBookDTO {
 
     @NotBlank(message = "Title is required")
+    @Schema(description = "Book title", example = "War and Peace")
     private String title;
 
     @NotBlank(message = "Author is required")
