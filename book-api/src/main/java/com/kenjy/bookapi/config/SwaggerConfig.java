862443c291fdfea969a914283546b305ad47c178
KenjyHD <calmisliviu@gmail.com>
2025-05-11 15:17:45 +0300

    Improved swagger documentation

--- book-api/src/main/java/com/kenjy/bookapi/config/SwaggerConfig.java
+++ book-api/src/main/java/com/kenjy/bookapi/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.kenjy.bookapi.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,20 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .components(
-                        new Components().addSecuritySchemes(BASIC_AUTH_SECURITY_SCHEME,
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
-                .info(new Info().title(applicationName));
+                .components(new Components()
+                        .addSecuritySchemes(BASIC_AUTH_SECURITY_SCHEME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")
+                                        .description("Basic authentication")
+                        )
+                )
+                .info(new Info()
+                        .title(applicationName)
+                        .version("1.0")
+                        .description("Book API Documentation")
+                )
+                .addSecurityItem(new SecurityRequirement().addList(BASIC_AUTH_SECURITY_SCHEME));
     }
 
     public static final String BASIC_AUTH_SECURITY_SCHEME = "basicAuth";
